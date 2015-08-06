a7a5db3cde326e4f5b29214e9a81845522a15661
chalpin <chalpin@salesforce.com>
2015-08-06 09:55:46 -0400

    Don't blast the bashrc out duh...

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,7 +5,7 @@ cd "$(dirname "${BASH_SOURCE}")";
 git pull origin master;
 
 function doIt() {
-    rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" --exclude ".bash_profile" \
+    rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" --exclude ".bashrc" \
         --exclude "README.md" --exclude "LICENSE-MIT.txt" -avh --no-perms . ~;
     source ~/.bashrc;
 }
@@ -16,7 +16,7 @@ else
     read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
     echo "";
     if [[ $REPLY =~ ^[Yy]$ ]]; then
-        read "Nice! Once the install finishes you'll have to init submodules in ~/ and update your bash profile";
+        read "Nice! Once the install finishes you'll have to init submodules in ~/ and update your .bashrc";
         doIt;
     fi;
 fi;
